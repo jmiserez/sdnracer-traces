01c3e2cb9626bbe76a589c78a87e9555309a1648
Ahmed El-Hassany <eahmed@ethz.ch>
2015-09-16 15:56:16 +0200

    buggy traces

---
+++ trace_pox_hb_reactive-consistencyMeshTopology-False-True-steps50/openflow_replay_config.py
@@ -0,0 +1,20 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow.openflow_replayer import OpenFlowReplayer
+from sts.simulation_state import SimulationConfig
+from sts.input_traces.input_logger import InputLogger
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd='./pox.py --verbose  forwarding.l2_fwd --consistent=False --use_barrier=True  openflow.of_01 --address=__address__ --port=__port__ ', label='c1', address='127.0.0.1', cwd='pox/')],
+                 topology_class=MeshTopology,
+                 topology_params="num_switches=2",
+                 patch_panel_class=BufferedPatchPanel,
+                 multiplex_sockets=False,
+                 ignore_interposition=False,
+                 kill_controllers_on_exit=True)
+
+control_flow = OpenFlowReplayer(simulation_config, "traces/trace_pox_hb_reactive-consistencyMeshTopology-False-True-steps50/events.trace")
+# wait_on_deterministic_values=False
+# delay_flow_mods=False
+# Invariant check: 'InvariantChecker.check_liveness'
+# Bug signature: ""
