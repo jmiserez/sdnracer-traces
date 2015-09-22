d8b831e3bd6b61a134ededa2969102e9d9a37e7b
Jeremie Miserez <jeremie@miserez.org>
2015-09-22 08:10:51 +0200

    floodlight circuitpusher, learningswitch, forwarding

---
+++ trace_floodlight_circuitpusher-BinaryLeafTreeTopology2-steps400/interactive_replay_config.py
@@ -0,0 +1,20 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow.interactive_replayer import InteractiveReplayer
+from sts.simulation_state import SimulationConfig
+from sts.input_traces.input_logger import InputLogger
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd='java -ea -Dlogback.configurationFile=./src/main/resources/logback-trace.xml -jar ./target/floodlight.jar -cf ./src/main/resources/trace_circuitpusher.properties', label='c1', address='127.0.0.1', cwd='../floodlight')],
+                 topology_class=BinaryLeafTreeTopology,
+                 topology_params="num_levels=2",
+                 patch_panel_class=BufferedPatchPanel,
+                 multiplex_sockets=False,
+                 ignore_interposition=False,
+                 kill_controllers_on_exit=True)
+
+control_flow = InteractiveReplayer(simulation_config, "traces/trace_floodlight_circuitpusher-BinaryLeafTreeTopology2-steps400/events.trace")
+# wait_on_deterministic_values=False
+# delay_flow_mods=False
+# Invariant check: 'InvariantChecker.check_liveness'
+# Bug signature: ""
---
+++ trace_floodlight_forwarding-BinaryLeafTreeTopology1-steps100/replay_config.py
@@ -0,0 +1,24 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow.replayer import Replayer
+from sts.simulation_state import SimulationConfig
+from sts.input_traces.input_logger import InputLogger
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd='java -ea -Dlogback.configurationFile=./src/main/resources/logback-trace.xml -jar ./target/floodlight.jar -cf ./src/main/resources/trace_forwarding.properties', label='c1', address='127.0.0.1', cwd='../floodlight')],
+                 topology_class=BinaryLeafTreeTopology,
+                 topology_params="num_levels=1",
+                 patch_panel_class=BufferedPatchPanel,
+                 multiplex_sockets=False,
+                 ignore_interposition=False,
+                 kill_controllers_on_exit=True)
+
+control_flow = Replayer(simulation_config, "traces/trace_floodlight_forwarding-BinaryLeafTreeTopology1-steps100/events.trace",
+                        input_logger=InputLogger(),
+                        wait_on_deterministic_values=False,
+                        allow_unexpected_messages=False,
+                        delay_flow_mods=False,
+                        default_dp_permit=False,
+                        pass_through_whitelisted_messages=False,
+                        invariant_check_name='InvariantChecker.check_liveness',
+                        bug_signature="")
