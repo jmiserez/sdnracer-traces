148c020bfa4a07289c9d20166c8a420107f60b46
Ahmed El-Hassany <eahmed@ethz.ch>
2015-11-05 17:31:44 +0100

    Add traces for running forwarding app on new POX

---
+++ trace_pox_eel_l2_multi-BinaryLeafTreeTopology1-steps200/replay_config.py
@@ -0,0 +1,24 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow.replayer import Replayer
+from sts.simulation_state import SimulationConfig
+from sts.input_traces.input_logger import InputLogger
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd='echo "no-op"', label='c1', address='192.168.56.1', cwd='pox/', controller_type='dummy')],
+                 topology_class=BinaryLeafTreeTopology,
+                 topology_params="num_levels=1",
+                 patch_panel_class=BufferedPatchPanel,
+                 multiplex_sockets=False,
+                 ignore_interposition=False,
+                 kill_controllers_on_exit=True)
+
+control_flow = Replayer(simulation_config, "traces/trace_pox_eel_l2_multi-BinaryLeafTreeTopology1-steps200/events.trace",
+                        input_logger=InputLogger(),
+                        wait_on_deterministic_values=False,
+                        allow_unexpected_messages=False,
+                        delay_flow_mods=False,
+                        default_dp_permit=False,
+                        pass_through_whitelisted_messages=False,
+                        invariant_check_name='InvariantChecker.check_liveness',
+                        bug_signature="")
---
+++ trace_pox_eel_l2_multi_fixed-StarTopology2-steps200/mcs_config.py
@@ -0,0 +1,22 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow.mcs_finder import EfficientMCSFinder
+from sts.invariant_checker import InvariantChecker
+from sts.simulation_state import SimulationConfig
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd='echo "no-op"', label='c1', address='192.168.56.1', cwd='pox/', controller_type='dummy')],
+                 topology_class=StarTopology,
+                 topology_params="num_hosts=2",
+                 patch_panel_class=BufferedPatchPanel,
+                 multiplex_sockets=False,
+                 ignore_interposition=False,
+                 kill_controllers_on_exit=True)
+
+control_flow = EfficientMCSFinder(simulation_config, "traces/trace_pox_eel_l2_multi_fixed-StarTopology2-steps200/events.trace",
+                                  wait_on_deterministic_values=False,
+                                  default_dp_permit=False,
+                                  pass_through_whitelisted_messages=False,
+                                  delay_flow_mods=False,
+                                  invariant_check_name='InvariantChecker.check_liveness',
+                                  bug_signature="")
