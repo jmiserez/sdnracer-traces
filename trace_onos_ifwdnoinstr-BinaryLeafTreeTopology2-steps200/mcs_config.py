da425ecaef7ac166aa27320f1fd552eca15eeb68
Jeremie Miserez <jeremie@miserez.org>
2015-11-13 23:15:41 +0100

    add onos Bin2 200 (without instrumentation)

---
+++ trace_onos_ifwdnoinstr-BinaryLeafTreeTopology2-steps200/mcs_config.py
@@ -0,0 +1,22 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow.mcs_finder import EfficientMCSFinder
+from sts.invariant_checker import InvariantChecker
+from sts.simulation_state import SimulationConfig
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd='tail -f "/home/jeremie/Applications/apache-karaf-3.0.3/data/log/karaf.log"', label='c1', address='127.0.0.1', cwd='./', controller_type='logfile')],
+                 topology_class=BinaryLeafTreeTopology,
+                 topology_params="num_levels=2",
+                 patch_panel_class=BufferedPatchPanel,
+                 multiplex_sockets=False,
+                 ignore_interposition=False,
+                 kill_controllers_on_exit=True)
+
+control_flow = EfficientMCSFinder(simulation_config, "traces/trace_onos_ifwdnoinstr-BinaryLeafTreeTopology2-steps200/events.trace",
+                                  wait_on_deterministic_values=False,
+                                  default_dp_permit=False,
+                                  pass_through_whitelisted_messages=False,
+                                  delay_flow_mods=False,
+                                  invariant_check_name='InvariantChecker.check_liveness',
+                                  bug_signature="")
