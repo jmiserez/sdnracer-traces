c4d0d964a5902e8f81d45bba319f3d10ea242f3b
Ahmed El-Hassany <eahmed@ethz.ch>
2016-02-04 11:25:10 +0100

    Add Network traces used in PLDI'15 paper

---
+++ trace_onos_ifwd-StarTopology2-steps200/replay_config.py
@@ -0,0 +1,24 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow.replayer import Replayer
+from sts.simulation_state import SimulationConfig
+from sts.input_traces.input_logger import InputLogger
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd='tail -f "/home/jeremie/Applications/apache-karaf-3.0.3/data/log/karaf.log"', label='c1', address='127.0.0.1', cwd='./', controller_type='logfile')],
+                 topology_class=StarTopology,
+                 topology_params="num_hosts=2",
+                 patch_panel_class=BufferedPatchPanel,
+                 multiplex_sockets=False,
+                 ignore_interposition=False,
+                 kill_controllers_on_exit=True)
+
+control_flow = Replayer(simulation_config, "traces/trace_onos-ifwd-StarTopology2-steps200/events.trace",
+                        input_logger=InputLogger(),
+                        wait_on_deterministic_values=False,
+                        allow_unexpected_messages=False,
+                        delay_flow_mods=False,
+                        default_dp_permit=False,
+                        pass_through_whitelisted_messages=False,
+                        invariant_check_name='InvariantChecker.check_liveness',
+                        bug_signature="")
---
+++ trace_pox_eel_l2_multi-StarTopology2-steps200/openflow_replay_config.py
@@ -0,0 +1,20 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow.openflow_replayer import OpenFlowReplayer
+from sts.simulation_state import SimulationConfig
+from sts.input_traces.input_logger import InputLogger
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd=' ./pox.py --verbose openflow.of_01 --address=__address__ --port=__port__  openflow.discovery forwarding.l2_multi_orig', label='c1', address='127.0.0.1', cwd='/home/ahassany/repos/pox/')],
+                 topology_class=StarTopology,
+                 topology_params="num_hosts=2",
+                 patch_panel_class=BufferedPatchPanel,
+                 multiplex_sockets=False,
+                 ignore_interposition=False,
+                 kill_controllers_on_exit=True)
+
+control_flow = OpenFlowReplayer(simulation_config, "traces/trace_pox_eel_l2_multi-StarTopology2-steps200/events.trace")
+# wait_on_deterministic_values=False
+# delay_flow_mods=False
+# Invariant check: 'InvariantChecker.check_liveness'
+# Bug signature: ""
