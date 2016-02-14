cf3146049d3586f1ad93283ac864b0a56f3890a6
Jeremie Miserez <jeremie@miserez.org>
2016-02-14 21:09:27 +0100

    Update update-this-vm.sh

--- vm-setup/update-this-vm.sh
+++ vm-setup/update-this-vm.sh
@@ -3,7 +3,7 @@
 cd /tmp
 rm -f /tmp/update-sdnracer-vm-latest.sh
 # download latest update script
-wget https://github.com/jmiserez/sdnracer-traces/blob/artifact/vm-setup/update-sdnracer-vm-latest.sh -O update-sdnracer-vm-latest.sh
+wget https://raw.githubusercontent.com/jmiserez/sdnracer-traces/artifact/vm-setup/update-sdnracer-vm-latest.sh -O update-sdnracer-vm-latest.sh
 # execute update
 /bin/bash /tmp/update-sdnracer-vm-latest.sh
 rm -f /tmp/update-sdnracer-vm-latest.sh
