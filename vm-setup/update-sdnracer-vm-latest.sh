632d3ff10fa3ebfa2813b3b46ea68f7e39aa1f23
Jeremie Miserez <jeremie@miserez.org>
2016-02-14 21:17:39 +0100

    VM scripts

--- vm-setup/update-sdnracer-vm-latest.sh
+++ vm-setup/update-sdnracer-vm-latest.sh
@@ -11,16 +11,11 @@ rm -f onos-hb.zip
 rm -f onos-app-samples-hb.zip
 rm -f reset-this-vm.sh
 
-rm -f PLDITutorial.html
-rm -rf PLDITutorial_files
-rm -f ~/Desktop/PLDITutorial.html
-
 wget https://github.com/jmiserez/sts/archive/hb.zip -O sts-hb.zip
 wget https://github.com/jmiserez/pox/archive/hb.zip -O pox-hb.zip
 wget https://github.com/jmiserez/pox/archive/eel-hb.zip -O pox-eel-hb.zip
 wget https://github.com/jmiserez/floodlight/archive/hb.zip -O floodlight-hb.zip
 wget https://github.com/jmiserez/sdnracer-traces/archive/artifact.zip -O sdnracer-traces-artifact.zip
-# wget http://s3.miserez.org/sdnracer-pldi/colin_scott-hassel-sts-31afb29fa667.zip
 wget https://bitbucket.org/colin_scott/hassel-sts/get/31afb29fa667.zip -O colin_scott-hassel-sts-31afb29fa667.zip
 wget https://github.com/jmiserez/onos/archive/hb.zip -O onos-hb.zip
 wget https://github.com/jmiserez/onos-app-samples/archive/hb.zip -O onos-app-samples-hb.zip
@@ -43,8 +38,8 @@ chmod +x 02_reproduce_all_paper_results.sh
 wget http://s3.miserez.org/sdnracer-pldi/03_generate_a_new_random_trace.sh
 chmod +x 03_generate_a_new_random_trace.sh
 
-wget https://github.com/jmiserez/sdnracer-traces/blob/artifact/vm-setup/update-this-vm.sh -O update-this-vm.sh
-wget https://github.com/jmiserez/sdnracer-traces/blob/artifact/vm-setup/reset-this-vm.sh -O reset-this-vm.sh
+wget http://s3.miserez.org/sdnracer-pldi/update-this-vm.sh -O update-this-vm.sh
+wget http://s3.miserez.org/sdnracer-pldi/reset-this-vm.sh -O reset-this-vm.sh
 chmod +x update-this-vm.sh
 chmod +x reset-this-vm.sh
 
--- vm-setup/update-this-vm.sh
+++ vm-setup/update-this-vm.sh
@@ -3,7 +3,7 @@
 cd /tmp
 rm -f /tmp/update-sdnracer-vm-latest.sh
 # download latest update script
-wget https://raw.githubusercontent.com/jmiserez/sdnracer-traces/artifact/vm-setup/update-sdnracer-vm-latest.sh -O update-sdnracer-vm-latest.sh
+wget http://s3.miserez.org/sdnracer-pldi/update-sdnracer-vm-latest.sh -O update-sdnracer-vm-latest.sh
 # execute update
 /bin/bash /tmp/update-sdnracer-vm-latest.sh
 rm -f /tmp/update-sdnracer-vm-latest.sh
